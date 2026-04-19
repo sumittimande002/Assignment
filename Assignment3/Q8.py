af146454c42ad5cd6a3a531b578359844cd0485b
sumittimande002 <sumittimande002@gmail.com>
2026-04-19 17:45:45 +0530

    Assignment Q10 Compl

--- Assignment3/Q8.py
+++ Assignment3/Q8.py
@@ -1,10 +1,22 @@
 # 8.Write a program to count the frequency of each element in a list using a dictionary.
 
-l = [10,20,30,50,10,3200,40,80,90,80,40,20,30]
+l = [10,20,30,50,10,32,40,80,90,80,40,20,30]
+l.sort()
 # print(type(l))
 # d = {"Values":(l)}
 # print(type(d))
 # print(d)
 # s = set(d.values())
 # print(s)
-fraq = 0
+
+
+fraq = {}
+for i in l:
+    if i in fraq:
+        fraq[i]+=1
+    else:
+        fraq[i] = 1
+
+print("Frequency of element is : ")
+for key ,value in fraq.items():
+    print(key ," : ",value)
