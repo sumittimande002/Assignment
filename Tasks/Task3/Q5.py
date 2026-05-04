209f1a22cedca9d154504dec96902edbd590fa52
sumittimande002 <sumittimande002@gmail.com>
2026-05-04 11:15:22 +0530

    Task3 Q5

--- Tasks/Task3/Q5.py
+++ Tasks/Task3/Q5.py
@@ -1,3 +1,28 @@
 ## 25. Program to Generate Fibonacci Series
 
 # **Problem Statement:** Write a program to generate Fibonacci series up to N terms.
+num  = int(input("Enter Number : "))
+# a , b = 0,1
+# for i in range (a,num):
+#     print(a , end=" ")
+#     c = a+b
+#     a = b
+#     b = c
+#     # a , b = b ,a+b
+
+
+
+
+
+
+
+
+
+
+a , b = 0,1
+
+for i in range (a,num):
+    print(a, end = " ")
+    c = a+ b
+    a = b
+    b = c
